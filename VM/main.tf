f8a34ad629a288f82aaa23d97876b9a744486b42
“Oanh <oanhngo9@yahoo.com>
2024-03-09 23:04:30 +0000

    added vm

---
+++ VM/main.tf
@@ -0,0 +1,26 @@
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
