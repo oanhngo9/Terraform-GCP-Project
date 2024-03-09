f4bc909a28e2d1b7e9e8e64d5b32a348b4ad1180
“Oanh <oanhngo9@yahoo.com>
2024-03-09 22:52:49 +0000

    updated name account

--- Account/main.tf
+++ Account/main.tf
@@ -1,37 +1,37 @@
 data "google_billing_account" "acct" {
-	display_name = "My Billing Account"
-	open = true
+  display_name = "My Billing Account"
+  open         = true
 }
 
 resource "random_password" "password" {
-	length = 16
-	numeric = false
-	special = false
-	lower = true
-	upper = false
+  length  = 16
+  numeric = false
+  special = false
+  lower   = true
+  upper   = false
 }
 
-resource "google_project" "terraform_gcp_project" {
-	name = "terraform_gcp_project"
-	project_id = random_password.password.result
-	billing_account = data.google_billing_account.acct.id
+resource "google_project" "terraform-gcp-project" {
+  name            = "terraform-gcp-project"
+  project_id      = random_password.password.result
+  billing_account = data.google_billing_account.acct.id
 }
 
 resource "null_resource" "set-project" {
-	triggers = {
-        always_run = "${timestamp()}"
-    }
-	
-	provisioner "local-exec" {
-	    command = "gcloud config set project ${google_project.terraform_gcp_project.project_id}"
-	}
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "gcloud config set project ${google_project.terraform_gcp_project.project_id}"
+  }
 }
 
 resource "null_resource" "unset-project" {
-	provisioner "local-exec" {
-	when = destroy
-	command = "gcloud config unset project"
-	}
+  provisioner "local-exec" {
+    when    = destroy
+    command = "gcloud config unset project"
+  }
 }
 
 
