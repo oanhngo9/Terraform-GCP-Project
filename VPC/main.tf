5ea0ccb743a60ab3aab0a2530987fa7b4a569b31
“Oanh <oanhngo9@yahoo.com>
2024-03-12 22:24:40 +0000

    removed vpc error

--- VPC/main.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_network" "gcp_vpc_network" {
-  name = "dec-vpc-network"
-  routing_mode = "GLOBAL"
-  auto_create_subnetworks = true
-}
-
-output "vpc_self_link" {
-  value = google_compute_network.gcp_vpc_network.self_link
-}
