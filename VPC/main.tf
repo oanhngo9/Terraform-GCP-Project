667125e0b2de2923d5c9d79cc9bee9d3d470f82c
“Oanh <oanhngo9@yahoo.com>
2024-03-12 00:22:05 +0000

    updated firewall

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,61 +1,31 @@
-resource "google_compute_autoscaler" "foobar" {
-  name   = "oanh-autoscaler"
-  zone   = "us-central1-f"
-  target = google_compute_instance_group_manager.foobar.id
-
-  autoscaling_policy {
-    max_replicas    = 5
-    min_replicas    = 1
-    cooldown_period = 60
-
-    cpu_utilization {
-      target = 0.5
-    }
-  }
+resource "google_compute_network" "vpc_network" {
+  name = "oanh-vpc-network"
+  routing_mode = "GLOBAL"
+  auto_create_subnetworks = true
 }
 
-resource "google_compute_instance_template" "foobar" {
-  name           = "oanh-instance-template"
-  machine_type   = "e2-medium"
-  can_ip_forward = false
-
-  tags = ["foo", "bar"]
-
-  disk {
-    source_image = data.google_compute_image.debian_9.id
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-  }
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "my-private-ip"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.self_link
 }
 
-resource "google_compute_target_pool" "foobar" {
-  name = "my-target-pool"
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc_network.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
 
-resource "google_compute_instance_group_manager" "foobar" {
-  name = "my-igm"
-  zone = "us-central1-f"
+resource "google_compute_firewall" "default" {
+  name    = "my-firewall-rule"
+  network = google_compute_network.vpc_network.self_link
 
-  version {
-    instance_template  = google_compute_instance_template.foobar.id
-    name               = "primary"
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
   }
 
-  target_pools       = [google_compute_target_pool.foobar.id]
-  base_instance_name = "foobar"
-}
-
-data "google_compute_image" "debian_9" {
-  family  = "debian-9"
-  project = "debian-cloud"
+  source_ranges = ["0.0.0.0/0"]
 }
