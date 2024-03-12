26288cf544c7790e7d5f4383ae580c65f6374168
“Oanh <oanhngo9@yahoo.com>
2024-03-12 21:12:29 +0000

    removed vpc

--- ASG/main.tf
+++ ASG/main.tf
@@ -1,10 +1,4 @@
 # Create ASG for the project
-resource "google_compute_network" "gcp_vpc_network" {
-  name = "gcp-vpc-network"
-  routing_mode = "GLOBAL"
-  auto_create_subnetworks = true
-}
-
 resource "google_compute_autoscaler" "asg" {
   provider = google-beta
   zone   = "us-central1-a"  
