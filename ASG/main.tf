1d6df4b9acc026cf268519bc48131b0ed8e3c647
“Oanh <oanhngo9@yahoo.com>
2024-03-12 23:36:27 +0000

    changed network not declared

--- ASG/main.tf
+++ ASG/main.tf
@@ -10,11 +10,11 @@ resource "google_compute_global_address" "private_ip_address" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = google_compute_network.network.self_link
+  network       = google_compute_network.dec_vpc_network.self_link
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
-  network                 = google_compute_network.network.self_link
+  network                 = google_compute_network.dec_vpc_network.self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]  
 }
