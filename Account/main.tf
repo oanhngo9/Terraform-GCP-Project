6280800dfd7cd17cf41edf6e570d0ba47f3e4e44
“Oanh <oanhngo9@yahoo.com>
2024-03-13 00:55:39 +0000

    return code to account

---
+++ Account/main.tf
@@ -0,0 +1,60 @@
+# Show Billing Account Infomation
+data "google_billing_account" "acct" {
+  display_name = "My Billing Account"
+  open         = true
+}
+
+# Generate 16 characters random password 
+resource "random_password" "password" {
+  length  = 16
+  numeric = false
+  special = false
+  lower   = true
+  upper   = false
+}
+
+# Create new Project_ID
+provider "random" {}
+
+resource "random_id" "project_id" {
+  byte_length = 12
+}
+
+resource "google_project" "gcp_team_project" {
+  name            = "gcp-team-project"
+  project_id      = "gcp-${random_id.project_id.hex}"
+  billing_account = data.google_billing_account.acct.id
+
+  depends_on = [random_id.project_id]
+}
+
+# Set terminal to the project
+resource "null_resource" "set_project" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "gcloud config set project ${google_project.gcp_team_project.project_id}"
+  }
+}
+
+# Enable list of services
+resource "null_resource" "enable-apis" {
+  depends_on = [
+    google_project.gcp_team_project,
+    null_resource.set_project
+  ]
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = <<-EOT
+        gcloud services enable compute.googleapis.com
+        gcloud services enable dns.googleapis.com
+        gcloud services enable storage-api.googleapis.com
+        gcloud services enable container.googleapis.com
+        gcloud services enable file.googleapis.com
+    EOT
+  }
+}
