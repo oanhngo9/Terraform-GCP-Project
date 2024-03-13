24c1f2d63e2d1a0bf71b3199743c35806af51c5c
“Oanh <oanhngo9@yahoo.com>
2024-03-13 00:16:48 +0000

    modify bash -c

--- ASG/apache.sh
+++ ASG/apache.sh
@@ -4,8 +4,8 @@ DB_USER=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attri
 DB_PASSWORD=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/db-password -H "Metadata-Flavor: Google")
 DOMAIN_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/domain-name -H "Metadata-Flavor: Google")
 sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-sudo sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
-sudo sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
-sudo sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wp-config.php
-echo "define('WP_HOME','http://$DOMAIN_NAME');" >> /var/www/html/wp-config.php
-echo "define('WP_SITEURL','http://$DOMAIN_NAME');" >> /var/www/html/wp-config.php}}}
+sudo bash -c "sed -i \"s/database_name_here/$DB_NAME/g\" /var/www/html/wp-config.php"
+sudo bash -c "sed -i \"s/username_here/$DB_USER/g\" /var/www/html/wp-config.php"
+sudo bash -c "sed -i \"s/password_here/$DB_PASSWORD/g\" /var/www/html/wp-config.php"
+sudo bash -c "echo \"define('WP_HOME','http://$DOMAIN_NAME');\" >> /var/www/html/wp-config.php"
+sudo bash -c "echo \"define('WP_SITEURL','http://$DOMAIN_NAME');\" >> /var/www/html/wp-config.php"
